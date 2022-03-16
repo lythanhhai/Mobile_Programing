184f8b6747c23cde326bfa986b0405035266158f
lythanhhai <you@example.com>
2022-03-17 03:15:19 +0700

    :ok_hand: add function for contact app

--- Project_Mobile/contactapp1911/src/main/java/com/example/contactapp1911/Add_Contact.java
+++ Project_Mobile/contactapp1911/src/main/java/com/example/contactapp1911/Add_Contact.java
@@ -2,20 +2,41 @@ package com.example.contactapp1911;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.contactapp1911.databinding.ActivityAddContactBinding;
 import com.example.contactapp1911.databinding.ActivityMainBinding;
 
 public class Add_Contact extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
+    ActivityAddContactBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        binding = ActivityAddContactBinding.inflate(getLayoutInflater());
         View viewRoot = binding.getRoot();
         setContentView(viewRoot);
 
+        Intent intent = getIntent();
+        if(intent != null)
+        {
+        }
+
+        binding.btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.putExtra("name", binding.etName.getText().toString());
+                intent.putExtra("phone", binding.etPhone.getText().toString());
+                intent.putExtra("email", binding.etEmail.getText().toString());
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+
     }
+
 }
--- Project_Mobile/contactapp1911/src/main/java/com/example/contactapp1911/MainActivity.java
+++ Project_Mobile/contactapp1911/src/main/java/com/example/contactapp1911/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.contactapp1911;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.loader.content.AsyncTaskLoader;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.SearchView;
 
 import com.example.contactapp1911.databinding.ActivityMainBinding;
@@ -25,6 +32,10 @@ public class MainActivity extends AppCompatActivity {
     private ContactAdapter contactsAdapterSearch;
     private AppDatabase appDatabase;
     private ContactDao contactDao;
+
+    ActivityResultLauncher<Intent> getInfoNewContact;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,14 +51,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 contactDao.nullTable();
-                Contact user1 = new Contact("Nguyen Van A", "1234565", "nva@gmail.com");
-                Contact user2 = new Contact("Nguyen Van B", "5345345", "nvb@gmail.com");
-                Contact user3 = new Contact("Nguyen Van C", "6235435", "nvc@gmail.com");
+                Contact user1 = new Contact("Nguyen Van A", "0945443534", "nva@gmail.com");
+                Contact user2 = new Contact("Ho Van B", "0845443454", "hvb@gmail.com");
+                Contact user3 = new Contact("Tran Van C", "0545465534", "tvc@gmail.com");
                 contactDao.insertAll(user1);
                 contactDao.insertAll(user2);
                 contactDao.insertAll(user3);
                 List<Contact> list = contactDao.getAllContacts();
-                //list.clear();
                 for(int i = 0; i < list.size(); i++)
                 {
                     contacts.add(list.get(i));
@@ -62,8 +72,55 @@ public class MainActivity extends AppCompatActivity {
         contactsAdapter = new ContactAdapter(contacts);
         binding.rvContact.setAdapter(contactsAdapter);
         binding.rvContact.setLayoutManager(new LinearLayoutManager(this));
+        // lấy thông tin add
+        getInfoNewContact = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK && result != null) {
+                            if(result.getData() != null && result.getData().getStringExtra("name") != null && result.getData().getStringExtra("phone") != null && result.getData().getStringExtra("email") != null)
+                            {
+                                Intent data = result.getData();
+                                // your operation....
+                                String name = data.getStringExtra("name");
+                                String phone = data.getStringExtra("phone");
+                                String email = data.getStringExtra("email");
+                                // Sử dụng kết quả result bằng cách hiện Toast
+                                binding.textView.setText("");
+                                //List<Contact> list1 = contactDao.getAllContacts();
+                                AsyncTask.execute(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                            //contactDao.nullTable();
+                                            Contact userContact = new Contact(name, phone, email);
+                                            contactDao.insertContact(userContact);
+                                            contacts.clear();
+                                            List<Contact> list = contactDao.getAllContacts();
+                                            for(int i = 0; i < list.size(); i++)
+                                            {
+                                                contacts.add(list.get(i));
+                                            }
+                                            binding.rvContact.setAdapter(contactsAdapter);
+                                        }
+                                });
+                            }
+                        }
+                    }
+                });
+
+
+
+        binding.btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Add_Contact.class);
+                getInfoNewContact.launch(intent);
+            }
+        });
 
         }
+
         @Override
         public boolean onCreateOptionsMenu(Menu menu) {
             MenuInflater inflater = getMenuInflater();
